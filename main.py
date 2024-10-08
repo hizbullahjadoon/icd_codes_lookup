a592b36abcf5f7e773e42186cd217706fc6e004f
Hizbullah Khan Jadoon <167857911+hizbullahjadoon@users.noreply.github.com>
2024-10-08 21:04:18 +0500

    Update main.py

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ import gc
 
 # Step 1: Initialize the PersistentClient and specify a directory for storing the data
 client = chromadb.PersistentClient(path="./chroma_persist")  # Specify your directory
-collection = client.get_collection("icd_codes_collection")
-'''
+collection = client.create_collection("icd_codes_collection")
+
 file_path = os.path.join(os.path.dirname(__file__), 'cleaned_icd_codes.csv')
 df = pd.read_csv(file_path)
 
@@ -54,7 +54,7 @@ for start_idx in range(last_index,len(df), batch_size):
 print(f"Processed up to index Exiting.")
 
 print("ICD codes have been added to the persistent collection successfully!")
-'''
+
 # Initialize the ChromaDB Persistent Client
 client = chromadb.PersistentClient(path="./chroma_persist")  # Path to your persistent storage
 collection = client.get_collection("icd_codes_collection")  # Load the existing collection
